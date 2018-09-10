86bcf8b697d148f44d8499fc3c59ac24cd6b4f83
Adrian <adrian@thgie.ch>
2018-09-10 09:12:09 +0200

    last feedback sonia

--- assets/ob.js
+++ assets/ob.js
@@ -275,6 +275,12 @@ function scrolling(){
 			}
 		}
 	}
+
+    if (getDocHeight() == getScrollXY()[1] + window.innerHeight) {
+        document.querySelector('.nav').classList.add('bottom')
+    } else {
+        document.querySelector('.nav').classList.remove('bottom')
+	}
 }
 
 scrolling()
@@ -403,3 +409,30 @@ window.addEventListener('resize', function() {
 });
 
 video_background()
+
+function getScrollXY() {
+    var scrOfX = 0, scrOfY = 0;
+    if( typeof( window.pageYOffset ) == 'number' ) {
+        //Netscape compliant
+        scrOfY = window.pageYOffset;
+        scrOfX = window.pageXOffset;
+    } else if( document.body && ( document.body.scrollLeft || document.body.scrollTop ) ) {
+        //DOM compliant
+        scrOfY = document.body.scrollTop;
+        scrOfX = document.body.scrollLeft;
+    } else if( document.documentElement && ( document.documentElement.scrollLeft || document.documentElement.scrollTop ) ) {
+        //IE6 standards compliant mode
+        scrOfY = document.documentElement.scrollTop;
+        scrOfX = document.documentElement.scrollLeft;
+    }
+    return [ scrOfX, scrOfY ];
+}
+
+function getDocHeight() {
+    var D = document;
+    return Math.max(
+        D.body.scrollHeight, D.documentElement.scrollHeight,
+        D.body.offsetHeight, D.documentElement.offsetHeight,
+        D.body.clientHeight, D.documentElement.clientHeight
+    );
+}
